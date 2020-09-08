0005700705ad5810be1b7d4d0876afbfffa8c2c6
Joseph Wenrich <joseph@ou.edu>
2020-09-08 18:06:54 -0500

    Finished in class example over WPF.

--- WPF examples/In class WPF/MainWindow.xaml.cs
+++ WPF examples/In class WPF/MainWindow.xaml.cs
@@ -23,6 +23,42 @@ namespace In_class_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            Txt_Favorite_Color.Text = string.Empty;
+            Txt_Enter_Name.Clear();
+            btnClickMe.IsEnabled = false;
+            //assign text as variable
+            var x = Txt_Favorite_Color.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var response=  MessageBox.Show("You clicked me!", "Enter title here", MessageBoxButton.YesNoCancel);
+            string message = $"Hey {Txt_Enter_Name.Text} that's cool that your favorite color is {Txt_Favorite_Color.Text}!";
+            MessageBox.Show(message, "!~Welcome to MIS 3033~!");
+        }
+
+        private void Txt_Favorite_Color_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //MessageBox.Show("You changed me!");
+        }
+
+        private void Txt_Enter_Name_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show("Key down!");
+
+        }
+
+        private bool ShouldWeEnableTheButton()
+        {
+            bool result = false;
+            if (Txt_Favorite_Color.Text != string.Empty
+                && Txt_Enter_Name.Text != string.Empty)
+            {
+                result = true;
+            }
+
+            return result;
         }
     }
 }
